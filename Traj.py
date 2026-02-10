64b86da1a7f4bcfcc87ca496cb0032eaf691453a
AmiroooTheWalnut <amir.esmaieeli@gmail.com>
2026-02-09 18:37:31 -0700

    Updating Traj.py file.

--- Traj.py
+++ Traj.py
@@ -1,11 +1,12 @@
 
 
 class SubTraj:
-    def __init__(self,initLat,initLon,cutLat,cutLon,points,isFromInit):
-        self.initLat=initLat
+    def __init__(self,initLat,initLon,cutLat,cutLon,points,isFromInit, isContinue):
+        self.initLat=initLat# always shows the original lat/lon space to distinguish the starting point
         self.initLon=initLon
         self.points=points
         self.isFromInit=isFromInit
-        self.cutLat=cutLat
+        self.cutLat=cutLat# always shows the starting location of a cut trajectory in normalized space
         self.cutLon=cutLon
         self.streetPoints=[]
+        self.isContinue=isContinue
